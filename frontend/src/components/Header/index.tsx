bdbab943a041f7a0d592ddaa81d054b90be8657c
WendelLorenzi <wlorenzi17@gmail.com>
2022-07-28 23:09:56 -0300

    feat: init refactor

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { Container, ContainerHeader, ContainerImage, ContainerButton } from './styles';
-import BruceHeader from '../../assets/bruceHeader.svg';
+// import BruceHeader from '../../assets/bruceHeader.svg';
 import React from 'react';
 import { Button } from '@mui/material';
 
@@ -7,16 +7,7 @@ const Header: React.FC = () => {
     return (
     <>
         <Container>
-            <ContainerHeader>
-                <ContainerImage>
-                    <BruceHeader />
-                </ContainerImage>
-                <ContainerButton>
-                    <Button variant="outlined">
-                        Produtos Batcaverna
-                    </Button>
-                </ContainerButton>
-            </ContainerHeader>
+            Batcaverna
         </Container>
     </>
     );
--- frontend/src/components/InitialBody/styles.ts
+++
@@ -1,9 +0,0 @@
-import { Box, styled, Card } from '@mui/material';
-
-export const Container: any = styled(Box)({
-    display: 'flex',
-    backgroundColor:  '#000000',
-});
-
-export const ContainerSimbolo: any = styled(Box)({
-});
